f83e2a491d03cfe64117b0d6977c9061c8c0ddb1
Craig Booker <craig@arcadia.software>
2018-04-28 21:24:05 -0500

    Defined the array variable for pages and initialized the variable in the onCreate function.

--- app/src/main/java/software/arcadia/interactivestory/StoryActivity.java
+++ app/src/main/java/software/arcadia/interactivestory/StoryActivity.java
@@ -3,8 +3,15 @@ package software.arcadia.interactivestory;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import software.arcadia.interactivestory.model.Page;
 
 public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Page[] pages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,17 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()){
+            name = "Friend";
+
+        }
+        Log.d(TAG, name);
+
+        // Initialize the pages array.
+        pages = new Page[7];
+
+        
+
     }
 }
