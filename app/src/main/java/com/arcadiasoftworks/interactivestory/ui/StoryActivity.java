6ad1d9ff5d52180d0e889a5aaa75c382cd0eb103
Craig Booker <craig@arcadia.software>
2018-05-02 09:41:12 -0500

    Relocated the file under the ui directory.

--- app/src/main/java/com/arcadiasoftworks/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/arcadiasoftworks/interactivestory/ui/StoryActivity.java
@@ -1,10 +1,11 @@
-package com.arcadiasoftworks.interactivestory;
+package com.arcadiasoftworks.interactivestory.ui;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.arcadiasoftworks.interactivestory.R;
 import com.arcadiasoftworks.interactivestory.model.Page;
 
 public class StoryActivity extends AppCompatActivity {
